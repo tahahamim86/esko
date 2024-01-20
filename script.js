88f51bcd7745b7c67af3e0a2c1d68753fa0ec8da
taha <99847753+tahahamim86@users.noreply.github.com>
2024-01-20 02:43:07 +0100

    adding alert in js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
  const menuToggle = document.querySelector('.toggle');
 const showcase = document.querySelector('.showcase');
-
+alert("hello");
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active');
   showcase.classList.toggle('active');
